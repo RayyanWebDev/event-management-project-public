2deb71abc95cd0d62e33905a7aa0286fed532b74
rayyan2300 <rayyan3379@gmail.com>
2023-10-09 20:32:30 +0600

    added details information when button is clicked

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,6 @@ import Services from "../Services/Services";
 const Home = () => {
   return (
     <div className="max-w-screen-2xl mx-auto">
-      <div>This is Home</div>
       <Banner></Banner>
       <Services></Services>
     </div>
--- src/Components/ServiceDetails/ServiceDetails.jsx
+++ src/Components/ServiceDetails/ServiceDetails.jsx
@@ -9,11 +9,33 @@ const ServiceDetails = () => {
 
   return (
     <div>
-      <img
-        className="mx-auto border-2 border-amber-600"
-        src={service.image}
-        alt=""
-      />
+      <div
+        className="hero min-h-screen "
+        style={{
+          backgroundImage: `url(${service.image})`,
+        }}
+      >
+        <div className="card flex-shrink-0 w-full max-w-2xl  bg-black backdrop-filter  bg-opacity-50  shadow-xl ">
+          <h2 className="mt-11">
+            <span
+              style={{ textTransform: "uppercase" }}
+              className="text-white  text-7xl font-bold "
+            >
+              {service.name}
+            </span>
+          </h2>
+          <h3 className="text-red-600 mb-10  text-7xl font-bold">
+            {" "}
+            ${service.price}
+          </h3>
+
+          <h3 className="text-white mb-6 text-2xl font-bold"> Details:</h3>
+          <h3 className="text-gray-100 mb-11  text-xl font-bold">
+            {" "}
+            {service.details}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
